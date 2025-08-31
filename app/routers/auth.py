3aa72fb937695d8f862826cd8469b5de18b70e8f
Naveed Latif <naveedlatifccw113@gmail.com>
2025-08-31 16:16:31 +0500

    added refresh token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status,Response,Request
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app import schemas, models, utils, oauth2
@@ -8,7 +8,7 @@ from typing import Optional
 router = APIRouter(
     tags=["auth"]
 )
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 async def login(login_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     # Find user by phone number
     user = db.query(models.User).filter(
@@ -21,4 +21,15 @@ async def login(login_data: OAuth2PasswordRequestForm = Depends(), db: Session =
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     access_token = oauth2.create_access_token(data={"user_id": user.id})
+    
+    refresh_token = oauth2.create_refresh_token(user.id, db)
+        
+        # Set refresh token as HTTP-only cookie
+    Response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            max_age=7*24*60*60,  # 7 days in seconds
+            samesite="lax"
+        )
     return {"access_token": access_token, "token_type": "bearer"}
--- app/schemas.py
+++ app/schemas.py
@@ -97,6 +97,11 @@ class UserResponse(BaseModel):
 
 class Token(BaseModel):
     access_token: str
+    refresh_token: str
+    token_type: str
+
+class AccessTokenResponse(BaseModel):
+    access_token: str
     token_type: str
 
 class TokenData(BaseModel):
